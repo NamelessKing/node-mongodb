3f5ead1444e19f79e3ad2ffd3a9f8a8958938f8d
NamelessKing <habibur.tanjin3@gmail.com>
2019-02-05 21:59:07 +0000

    code optimized with Promises

--- operations.js
+++ operations.js
@@ -2,36 +2,20 @@ const assert = require("assert");
 
 exports.insertDocument = (db, doc, collection, callBack) => {
     const coll = db.collection(collection);
-    coll.insert(doc, (err, result) => {
-        assert.equal(err, null);
-        console.log("Inserted " + result.result.n +
-            " documents into the collection " + collection);
-        callBack(result);
-    });
+    return coll.insert(doc);
 };
 
 exports.findDocuments = (db, collection, callBack) => {
     const coll = db.collection(collection);
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-        callBack(docs);
-    });
+    return coll.find({}).toArray();
 };
 
 exports.removeDocument = (db, doc, collection, callBack) => {
     const coll = db.collection(collection);
-    coll.deleteOne(doc, (err, result) => {
-        assert.equal(err, null);
-        console.log("Removed the document " + doc);
-        callBack(result);
-    });
+    return coll.deleteOne(doc);
 };
 
 exports.updateDocument = (db, doc, update, collection, callBack) => {
     const coll = db.collection(collection);
-    coll.updateOne(doc, { $set: update }, null, (err, result) => {
-        assert.equal(err, null);
-        console.log("Updated the document with ", update);
-        callBack(result);
-    });
+    return coll.updateOne(doc, { $set: update }, null);
 };
